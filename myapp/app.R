af6f064f54aa24ff1556a01da2e078b30238191c
clintko <kuei.yueh.ko@duke.edu>
2025-04-17 00:35:51 -0400

    fix problems

--- myapp/app.R
+++ myapp/app.R
@@ -1,18 +1,19 @@
 library(tidyverse)
 library(shiny)
 
-### import data
+# Load data
 txt_furl = "https://www.dropbox.com/scl/fi/1sdttdmk29fxjduw1pe3x/fcc_starrmpracrispr_vote2.umap.chipseq_score.tsv?rlkey=jskwb0hxuvxwdo5evd6qjjvnm&raw=1"
-
 dat_region_umap = read_tsv(txt_furl)
-vec_txt_feature = setdiff(colnames(dat), c("UMAP1", "UMAP2", "Region", "Chrom", "ChromStart", "ChromEnd"))
 
-### setup UI
+# Get feature names
+vec_txt_feature = setdiff(colnames(dat_region_umap), c("UMAP1", "UMAP2", "Region", "Chrom", "ChromStart", "ChromEnd"))
+
+# UI
 ui = fluidPage(
     titlePanel("UMAP Embedding of CREs based on ChIP-seq Signals"),
     sidebarLayout(
         sidebarPanel(
-            selectInput("txt_feature", "ChIP-seq Factor", choices = vec_txt_feature, selected = "H3K4me3"),
+            selectInput("txt_feature", "ChIP-seq Factor", choices = vec_txt_feature, selected = "H3K4me3")
         ),
         mainPanel(
             plotOutput("plot_fcc_umap")
@@ -20,13 +21,13 @@ ui = fluidPage(
     )
 )
 
-### setup Server
+# Server
 server = function(input, output) {
     output$plot_fcc_umap <- renderPlot({
-        ggplot(dat_region_umap, aes_string(x = input$UMAP1, y = input$UMAP2, color = input$txt_feature)) +
+        ggplot(dat_region_umap, aes_string(x = "UMAP1", y = "UMAP2", color = input$txt_feature)) +
             geom_point(alpha = 0.5) +
             theme_minimal()
-  })
+    })
 }
 
 shinyApp(ui, server)
